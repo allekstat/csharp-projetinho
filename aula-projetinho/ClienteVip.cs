a1b275f02beb9fc0a59068eb060ccf540fa37775
Alexsander <alexsander.santana2@etec.sp.gov.br>
2025-04-22 22:21:52 -0300

    inicial

---
+++ aula-projetinho/ClienteVip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_projetinho
+{
+    internal class ClienteVip : Cliente
+    {
+        protected double desconto;
+        protected double cashback;
+        public void exibirCashBack() { }
+        public void calcularCashBack() { }
+    }
+}
---
+++ aula-projetinho/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_projetinho
+{
+    internal class Pessoa
+    {
+        protected string nome;
+        protected string email;
+        public void exibirDados() { }
+        public virtual void cadastrar() { }
+    }
+}
